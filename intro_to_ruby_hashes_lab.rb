9a339ba75e362b46237f8dcd63efbb494ec3f6f4
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-02 16:46:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,6 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  my_hash_creator = {key => value }
   # return a hash that includes the key and value parameters passed into this method
 end
 
