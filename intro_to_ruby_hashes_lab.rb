e31343d2aa189448ab93fda9eec9dac0785cc7ec
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-02 17:11:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,9 @@ end
 
 def update_counting_hash(hash, key)
   if (hash[key] != None):
-    hash[key] += 1
-  else
     hash[key] = 1
+  else
+    hash[key] += 1
   end
     # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
